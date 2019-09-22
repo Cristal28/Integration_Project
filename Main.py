dd92e8c40db206eedcd37c2f3f11da989fcc14c4
cristal28 <cramos0295@eagle.fgcu.edu>
2019-09-22 19:19:48 -0400

    New

--- Main.py
+++ Main.py
@@ -1,6 +1,10 @@
 #Cristal Ramos
-#A Game
-print("Welcome to my game!")
-      #hdhjshh
-      print("Idk")
-      print("IS this showing up")
+#TRIVIA???
+print("Welcome to trivia quiz!")
+print("I hope you know your Marvel questions!")
+
+print(float(input("What year the first Marvel comic book released?")))
+if 1939:
+      print("You are correct!")
+else:
+      print("You are wrong!")
